e2070fb95a7bc50eabc5bcf70894fc0bd45eb67f
Guilherme <guimenoci@gmail.com>
2024-03-29 17:26:31 -0300

    feature: criando metodo para registrar usuario

--- backend/src/main/java/com/br/login/controller/AuthController.java
+++ backend/src/main/java/com/br/login/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.br.login.controller;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.login.dto.LoginRequestDTO;
+import com.br.login.dto.RegisterRequestDTO;
 import com.br.login.dto.ResponseDTO;
 import com.br.login.entity.UserEntity;
 import com.br.login.infra.TokenService;
@@ -33,6 +36,25 @@ public class AuthController {
 		if(passwordEncoder.matches(user.getPassword(), body.password())) {
 			String token = this.tokenSerivce.generateToken(user);
 			return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+		}
+		
+		return ResponseEntity.badRequest().build();
+		
+	}
+	
+	@PostMapping("/register")
+	public ResponseEntity<?> register(@RequestBody RegisterRequestDTO body){
+		
+		Optional<UserEntity> user = this.repository.findByEmail(body.email());
+		if(user.isEmpty()) {
+			UserEntity newUser = new UserEntity();
+			newUser.setPassword(passwordEncoder.encode(body.password()));
+			newUser.setEmail(body.email());
+			newUser.setName(body.name());
+			repository.save(newUser);
+			
+			String token = this.tokenSerivce.generateToken(newUser);
+			return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
 		}
 		
 		return ResponseEntity.badRequest().build();
---
+++ backend/src/main/java/com/br/login/dto/RegisterRequestDTO.java
@@ -0,0 +1,5 @@
+package com.br.login.dto;
+
+public record RegisterRequestDTO(String name, String email, String password) {
+
+}
