683dbbe82f0ff7a862cb146b799714cf4312ed3b
Guilherme <guimenoci@gmail.com>
2024-03-29 16:56:25 -0300

    feature: criando a classe security filter

--- backend/src/main/java/com/br/login/repository/UserRepository.java
+++ backend/src/main/java/com/br/login/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.br.login.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.br.login.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, String>{
 
+	Optional<UserEntity> findByEmail(String email);
+	
 }
